fe2cd2986877aa5f3be7bba75b7a090c0538e436
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-01-22 13:32:37 +0200

    Implement game loop, if user wants to play again

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,4 +1,5 @@
 import random
+
 rock = '''
     _______
 ---'   ____)
@@ -38,7 +39,7 @@ _   _ ____ _  _    _ _ _ _ _  _   /
   |   |__| |__|    |_|_| | | \| .  
                                    
 '''
-lose_massage = '''
+lose_message = '''
 _   _ ____ _  _    _    ____ ____ ____   /
  \_/  |  | |  |    |    |  | [__  |___  / 
   |   |__| |__|    |___ |__| ___] |___ .  
@@ -52,45 +53,60 @@ _ ___ . ____    ____    ___  ____ ____ _ _ _   /
 '''
 
 print(welcome_message)
-flag = False
 drawings = [rock, paper, scissors]
-player_choice = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n")
 
-while not player_choice.isdigit():
-    print("Wrong input! Try again.")
 
-    player_choice = input("Type 0 for Rock, 1 for Paper or 2 for Scissors.\n")
+def game_loop():
+    flag = False
+    draw_flag = False
+    player_choice = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n")
 
-while True:
-    if player_choice in ["0", "1", "2"]:
-        player_choice = int(player_choice)
-        player_drawing = drawings[player_choice]
-        print(f"You choose:")
-        print(player_drawing)
-    else:
+    while not player_choice.isdigit():
         print("Wrong input! Try again.")
+
         player_choice = input("Type 0 for Rock, 1 for Paper or 2 for Scissors.\n")
-        continue
 
-    computer_choice = random.randint(0, len(drawings) - 1)
-    computer_drawing = drawings[computer_choice]
-    print(f"Computer choose:")
-    print(computer_drawing)
+    while True:
+        if player_choice in ["0", "1", "2"]:
+            player_choice = int(player_choice)
+            player_drawing = drawings[player_choice]
+            print(f"You choose:")
+            print(player_drawing)
+        else:
+            print("Wrong input! Try again.")
+            player_choice = input("Type 0 for Rock, 1 for Paper or 2 for Scissors.\n")
+            continue
 
-    if player_choice == computer_choice:
-        print(draw_message)
+        computer_choice = random.randint(0, len(drawings) - 1)
+        computer_drawing = drawings[computer_choice]
+        print(f"Computer choose:")
+        print(computer_drawing)
+
+        if player_choice == computer_choice:
+            draw_flag = True
+
+        if player_choice == 0 and computer_choice == 2:
+            flag = True
+        elif player_choice == 1 and computer_choice == 0:
+            flag = True
+        elif player_choice == 2 and computer_choice == 1:
+            flag = True
+
+        if flag:
+            print(win_message)
+        elif draw_flag:
+            print(draw_message)
+        else:
+            print(lose_message)
+
+        print("Do you want another game?")
+        new_game = input("Choose Y or N\n").upper()
+
+        if new_game == "Y":
+            game_loop()
+        else:
+            print("Thanks for playing!")
         break
 
-    if player_choice == 0 and computer_choice == 2:
-        flag = True
-    elif player_choice == 1 and computer_choice == 0:
-        flag = True
-    elif player_choice == 2 and computer_choice == 1:
-        flag = True
 
-    if flag:
-        print(win_message)
-    else:
-        print(lose_massage)
-
-    break
+game_loop()
